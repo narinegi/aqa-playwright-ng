d3d33932687212250654af76038e3fcdd6ca4c75
Narine <narinegasparian1@gmail.com>
2024-04-11 16:02:38 +0100

    hwconfig

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,21 @@
+// @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import testsConfig from "./config/config.js";
+import dotenv from 'dotenv'
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+dotenv.config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 const config = defineConfig({
-  testDir: './tests',
+  // testDir: './tests',
+  // testMatch: '/tests/**/*.spec.js',
+  testMatch: /tests\/.*\/*.spec.js/,
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,13 +28,10 @@ const config = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: true,
+    headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://qauto.forstudy.space/',
-    httpCredentials: {
-      username: 'guest',
-      password: 'welcome2qauto'
-    },
+    baseURL: testsConfig.baseUrl,
+    httpCredentials: testsConfig.httpCredentials,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     screenshot: 'on'
